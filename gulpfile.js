4b0e63206ed30c66cbb016724e1881e5a732e08f
Grímur Daníelsson <grimur.danielsson@gmail.com>
2015-03-20 01:09:24 +0000

    Working on gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var gulp = require('gulp');
+var minimist = require('minimist');
+var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
+var colors = require('colors');
+var moment = require('moment');
+
+gulp.paths = {
+    src: 'src',
+    tests: 'tests'
+};
+
+gulp.task('tests', function(done) { UnitTests(done) });
+
+function UnitTests(done) {
+    var argv = minimist(process.argv.slice(2));
+    var auto = argv.auto ? argv.auto : false;
+
+    if(auto === true) {
+        console.log('Unit Tests will be run automatically on file change!'.bold.blue);
+        gulp.watch([gulp.paths.tests + '/**', gulp.paths.src + '/**'], function() {
+            runTests(argv);
+        });
+    }
+
+    runTests(argv);
+}
+
+function runTests(argv) {
+
+    var coverage = argv.coverage ? argv.coverage : false;
+    var reporters;
+
+    if(typeof(coverage) === 'boolean') {
+        reporters = ['html'];
+    } else {
+        reporters = coverage.split(',');
+    }
+
+    console.log('Running Mocha Unit Tests'.bold, '-' ,moment(Date.now()).format('MM/DD HH:mm:ss'));
+    gulp.src([gulp.paths.src + '/**.js'])
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire())
+        .on('finish', function() {
+            gulp.src([gulp.paths.tests + '/**.spec.js'])
+            .pipe(mocha())
+            .pipe(istanbul.writeReports({ reporters: reporters , reportOpts: { dir: 'reports/coverage/'}}));
+        });
+}
---
+++ tests/lol.spec.js
@@ -0,0 +1,15 @@
+
+beforeEach(function() {
+  global.expect = require('chai').expect;
+  global.sinon = require('sinon');
+});
+
+describe('test', function() {
+	it('is test', function() {
+		expect(true).to.equal(true);
+		expect(true).to.equal(true);
+		expect(true).to.equal(true);
+	})
+
+	
+})
